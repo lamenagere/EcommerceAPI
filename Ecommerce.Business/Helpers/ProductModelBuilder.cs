d73860aea3414558ad3dfbe3e041ddc2d7aa4c08
Ascalon Ascalon <ascalons@hotmail.com>
2019-03-29 14:23:47 +0100

    First Commit

---
+++ Ecommerce.Business/Helpers/ProductModelBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.Models;
+using Ecommerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Business.Helpers
+{
+    public static class ProductModelBuilder
+    {
+        public static ProductModel Create(Product product)
+        {
+            if (product == null) return null;
+
+            return new ProductModel
+            {
+                id = product.Id,
+                name = product.Name,
+                price = product.Price,
+                description = product.Description,
+                publicationDate = product.PublicationDate,
+                categoryId = product.Category.Id,
+                categoryName = product.Category.Name
+            };
+        }
+    }
+}
---
+++ Ecommerce.Common/Models/CategoryHierarchyModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class CategoryHierarchyModel
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public List<CategoryHierarchyModel> subCategories { get; set; }
+    }
+}
